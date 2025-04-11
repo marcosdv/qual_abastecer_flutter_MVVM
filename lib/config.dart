b502329213dcff4dae541e55bebcc9f86399d7c9
marcosvendramini <marcosvendramini@hotmail.com>
2025-04-11 20:23:24 -0300

    Criando constantes

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  static String TituloApp = 'Qual Abastecer?';
+  static Color corPrincipal = Color(0xFF0B4D0D);
+  static Color corTextoTitulo = Color(0xFFFFFFFF);
+}
